deff30755c3c27f3d1464a6619833f10f0361ae0
Håkon Bogen <hakon.bogen@shortcut.no>
2014-04-08 18:48:51 +0200

    initial commit

---
+++ UITableView DataSource.xctemplate/___FILEBASENAME___.m
@@ -0,0 +1,35 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#import "___FILEBASENAME___.h"
+
+@interface ___FILEBASENAMEASIDENTIFIER___()
+
+@end
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+#warning uncomplete implementation
+    return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#(NSString *)#> forIndexPath:indexPath];
+}
+
+
+
+@end
