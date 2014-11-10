737523e469bbc5a9d966430dc4b04ef81b7a03ff
Håkon Bogen <hakon.bogen@shortcut.no>
2014-11-10 10:57:28 +0100

    adjust for swift 1.1

--- DataSource.xctemplate/UITableViewSwift/___FILEBASENAME___.swift
+++ DataSource.xctemplate/UITableViewSwift/___FILEBASENAME___.swift
@@ -10,15 +10,15 @@ import UIKit
 
 class ___FILEBASENAMEASIDENTIFIER___: NSObject, UITableViewDataSource {
     
-    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
     }
     
-    func  numberOfSectionsInTableView(tableView: UITableView!) -> Int {
+    func  numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
     
-    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as UITableViewCell
         configureCell(cell, indexPath: indexPath)
         return cell
