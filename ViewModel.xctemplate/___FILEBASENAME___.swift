e60f077404047e1d4b0049da66cad7157c8cdb9b
Håkon Bogen <bogen@beiningbogen.no>
2017-09-22 00:32:26 +0200

    Remove all outdated templates and add new for ViewModel

---
+++ ViewModel.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,36 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+import ReactiveCocoa
+import ReactiveSwift
+import Result
+
+protocol ___VARIABLE_productName___Inputs {
+
+}
+
+protocol  ___VARIABLE_productName___Outputs {
+
+}
+
+protocol ___VARIABLE_productName___Type {
+
+    var inputs: ___VARIABLE_productName___Inputs { get }
+    var outputs: ___VARIABLE_productName___Outputs { get }
+
+}
+
+class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_productName___Type, ___VARIABLE_productName___Inputs, ___VARIABLE_productName___Outputs {
+
+    init() {
+
+    }
+
+    var inputs: ___VARIABLE_productName___Inputs { return self }
+    var outputs: ___VARIABLE_productName___Outputs { return self }
+}
